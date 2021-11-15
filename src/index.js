a8bf5538b3ebfd21e3632bf88400d3088f62a652
cFadness <codie1989@gmail.com>
2021-11-15 10:49:16 -0800

    Create all basic files and install React and axios.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './components/App';
+import axios from 'axios';
+
+reactDom.render(<App />, document.getElementById('root'));
